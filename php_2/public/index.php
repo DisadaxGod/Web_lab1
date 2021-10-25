92fa2b286a1eb8a9c4b666d34d8924e1483f6964
Daniil <danil00002002@mail.ru>
2021-10-26 00:16:02 +0800

    so hard

--- php_2/public/index.php
+++ php_2/public/index.php
@@ -16,21 +16,29 @@
     }elseif (preg_match("#^/wolverine#", $url)) {
         $title = "Росомаха";
         $template = "__object.twig";
-        $context['image'] = "/images/wolf.jpg";
+        $context['name'] = "wolverine";
         if (preg_match("#^/wolverine/image#", $url)){
             $template = "image.twig";
+            $context['image'] = "/images/wolf.jpg";
+            $context['type'] = "image";
         } elseif(preg_match("#^/wolverine/info#", $url)){
             $template = "wolf_info.twig";
+            $context['text'] = "/images/wolf_info.php";
+            $context['type'] = "text";
         }
         
     }elseif (preg_match("#^/quicksilver#", $url)) {
         $title = "Ртуть";
         $template = "__object.twig";
-        $context['image'] = "/images/silver.jpg";
+        $context['name'] = "quicksilver";
         if (preg_match("#^/quicksilver/image#", $url)){
             $template = "image.twig";
+            $context['image'] = "/images/silver.jpg";
+            $context['type'] = "image";
         } elseif(preg_match("#^/quicksilver/info#", $url)){
             $template = "silver_info.twig";
+            $context['text'] = "/images/silver_info.php";
+            $context['type'] = "text";
         }
     }
     
@@ -42,10 +50,14 @@
         [
             "title" => "Росомаха",
             "url" => "/wolverine",
+            "url-image" => "/wolverine/image",
+            "url-info" => "/wolverine/info",
         ],
         [
             "title" => "Ртуть",
             "url" => "/quicksilver",
+            "url-image" => "/quicksilver/image",
+            "url-info" => "/quicksilver/info",
         ]
     ];   
 
